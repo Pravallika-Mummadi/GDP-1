f09972fa666bb2f7f6185198871dbfde5055d1bb
Jyothsna Naredla <123060113+Jyothsnsa@users.noreply.github.com>
2023-12-01 23:04:35 -0600

    By Jyothsna Naredla
    Worked on Firebase Connection and Design (Co-ordinated with Hemanth)

---
+++ ButtonExtension.swift
@@ -0,0 +1,12 @@
+
+
+import UIKit
+
+class ButtonLayerSetup: UIButton{
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+}
