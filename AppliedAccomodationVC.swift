752564291aecc8d2a074bd7c95f0a867ce7e4fc6
Pravallika Mummadi <122918549+Pravallika-Mummadi@users.noreply.github.com>
2024-04-07 22:27:48 -0500

    AppliedAccomodationVC - Pravallika Mummadi

--- AppliedAccomodationVC.swift
+++ AppliedAccomodationVC.swift
@@ -1,29 +1,37 @@
 import UIKit
 
- 
 class AppliedAccomodationVC: UIViewController {
-
+    // MARK: - Outlets
     @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Properties
     var bookings = [AppliedAccomodation]()
-
+    
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTableView()
+        loadData()
+    }
+    
+    // MARK: - Setup
+    private func setupTableView() {
         tableView.registerCells([EventListCell.self])
         tableView.delegate = self
         tableView.dataSource = self
-        loadData()
     }
-
-    func loadData() {
-        FireStoreManager.shared.getMyAppliedAccomodation { acc in
-            self.bookings = acc
-            self.tableView.reloadData()
+    
+    // MARK: - Data
+    private func loadData() {
+        FireStoreManager.shared.getMyAppliedAccomodation { [weak self] acc in
+            self?.bookings = acc
+            self?.tableView.reloadData()
         }
     }
 }
 
+// MARK: - UITableViewDelegate & UITableViewDataSource
 extension AppliedAccomodationVC: UITableViewDelegate, UITableViewDataSource {
-
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return bookings.count
     }
@@ -32,12 +40,11 @@ extension AppliedAccomodationVC: UITableViewDelegate, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "EventListCell", for: indexPath) as! EventListCell
         let booking = bookings[indexPath.row]
 
-      
         let title = "Booking \(indexPath.row + 1)"
         let desc = """
-            Status: \(booking.status ?? "")\n
-            Move-In Date: \(booking.moveInDate ?? "")\n
-            Property ID: \(booking.propertyId ?? "")\n
+            Status: \(booking.status ?? "")
+            Move-In Date: \(booking.moveInDate ?? "")
+            Property ID: \(booking.propertyId ?? "")
             """
 
         cell.setData(title: title, desc: desc)
@@ -45,20 +52,16 @@ extension AppliedAccomodationVC: UITableViewDelegate, UITableViewDataSource {
         return cell
     }
 
-  
-
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
         let booking = self.bookings[indexPath.row]
         
-        FireStoreManager.shared.getAccomodationDetails(documentId: booking.propertyId!) { data in
-             
-            let vc = self.storyboard?.instantiateViewController(withIdentifier:  "AccomodationDetailVC" ) as! AccomodationDetailVC
-             selectedAccomodation =  data
-            vc.moveFromHistory = true 
-            self.navigationController?.pushViewController(vc, animated: true)
+        FireStoreManager.shared.getAccomodationDetails(documentId: booking.propertyId!) { [weak self] data in
+            guard let self = self else { return }
             
+            let vc = self.storyboard?.instantiateViewController(withIdentifier:  "AccomodationDetailVC" ) as! AccomodationDetailVC
+            selectedAccomodation =  data
+            vc.moveFromHistory = true
+            self.navigationController?.pushViewController(vc, animated: true)
         }
-       
     }
 }
