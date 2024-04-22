51d040ad32eb267000988105c3978832cc386e41
Lalith Vuppala <123031115+Lalith143kl@users.noreply.github.com>
2024-04-21 19:47:39 -0500

    Lalith Vuppala

--- UIColor.swift
+++ UIColor.swift
@@ -5,7 +5,9 @@ extension UIColor {
         return UIColor.systemIndigo
     }
     
-     
+    static var greenColor: UIColor {
+        return UIColor(hex: 0x63C694)
+    }
     
     convenience init(hex: Int, alpha: CGFloat = 1.0) {
         let red = CGFloat((hex >> 16) & 0xFF) / 255.0
